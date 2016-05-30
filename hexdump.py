8b2430818db443bba432e73ea15c99211a688b82
Dmytro S <dmytrish@gmail.com>
2016-05-30 22:43:31 +0300

    Python style cleanup: hexdump.py

--- hexdump.py
+++ hexdump.py
@@ -8,26 +8,31 @@ addr_format = '%8x : '
 byte_format = '%2x '
 byte_fmt_len = 3
 
+
 def hexdump(raw_string, start_at=0):
     i = start_at
     s = ''
     asc = ''
-    
+
     # from the first line
     initial_offset = (start_at % bytes_per_line)
-    if initial_offset: 
+    if initial_offset:
         offset = byte_fmt_len * initial_offset
         offset += initial_offset / bytes_per_block
-        if initial_offset % bytes_per_block: offset += 1
+        if initial_offset % bytes_per_block:
+            offset += 1
         s = ' ' * offset
         asc = ' ' * initial_offset
-    
+
     for b in raw_string:
-        if 0 == i % bytes_per_block: s += ' '
+        if 0 == i % bytes_per_block:
+            s += ' '
         s += '%02x ' % ord(b)
 
-        if b in string.printable and ord(b) >= 0x10: asc += b
-        else: asc += nonprintable
+        if b in string.printable and ord(b) >= 0x10:
+            asc += b
+        else:
+            asc += nonprintable
 
         i += 1
 
@@ -36,7 +41,7 @@ def hexdump(raw_string, start_at=0):
             print (addr_format % (i - bytes_per_line)) + s
             s = ''
             asc = ''
-    
+
     finish_offset = i % bytes_per_line
     if finish_offset:
         offset = byte_fmt_len * (bytes_per_line - finish_offset)
